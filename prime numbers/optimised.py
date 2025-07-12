4ed14ff2cf543e704e51fe1cc7eb80c4726328ea
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-07-12 12:34:16 +1000

    optimised primes

--- prime numbers/optimised.py
+++ prime numbers/optimised.py
@@ -5,9 +5,10 @@ def get_primes_to_n(n: int) -> list[int]:
     output: list[int] = []
     for i in range(2,n+1):
         i_is_prime = True
-        for j in range(2,i):
+        for j in range(2,int(math.sqrt(i))+1):
             if i % j == 0:
                 i_is_prime = False
+                break
         if i_is_prime:
             output.append(i)
     return output
