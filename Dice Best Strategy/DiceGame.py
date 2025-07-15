91d9937606ef4f1e4234dd41fbde4960908adc7c
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-07-15 18:47:22 +1000

    dice game more

--- Dice Best Strategy/DiceGame.py
+++ Dice Best Strategy/DiceGame.py
@@ -2,14 +2,35 @@ import random as r
 
 class DiceGame:
     def __init__(self) -> None:
-        self.record: list[int] = []
+        self.face_record: list[int] = []
+        self.cash_record: list[int] = []
+        self.cash: int = 0
 
-    def roll(self) -> int:
+    def roll_and_result(self) -> bool:
         face: int = r.randint(1,6)
-        self.record.append(face)
-        return face
+        self.face_record.append(face)
+        
+        if face != 6:
+            self.cash += face
+            self.cash_record.append(self.cash)
+            return True # means game is still on
+        else:
+            self.cash = 0
+            self.cash_record.append(self.cash)
+            return False
+    
+    def get_face_record(self) -> list[int]:
+        return self.face_record
+
+    def get_cash_record(self) -> list[int]:
+        return self.cash_record
+
+
+
+        
     
 if __name__ == "__main__":
-    dice = DiceGame()
-    print(dice.roll())
+    dice: DiceGame = DiceGame()
+    
+    
 
