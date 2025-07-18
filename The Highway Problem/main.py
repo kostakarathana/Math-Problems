e2d1185057c6e1dedba8969e2db155e2759360eb
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-07-18 10:21:32 +1000

    braking changes

--- The Highway Problem/main.py
+++ The Highway Problem/main.py
@@ -10,19 +10,19 @@ class Highway:
     def __init__(self, speed_limit: int, lanes: int = 4) -> None:
         self.speed_limit = speed_limit  # in km/h
         self.lanes = lanes
-
-    def calculate_max_cars(self, time_period_h: float = 1.0) -> int:
-        speed_m_s = self.speed_limit * 1000 / 3600
+        self.speed_m_s = self.speed_limit * 1000 / 3600
 
 
-        braking_distance = 0.00577381*self.speed_limit**2 + 0.385119*self.speed_limit + 1.23214 # dry conservative
+        self.braking_distance = 0.00577381*self.speed_limit**2 + 0.385119*self.speed_limit + 1.23214 # dry conservative
         # braking_distance = 0.00791667*self.speed_limit**2 + 0.430357*self.speed_limit - 0.0892857 # wet conservative
-        # braking_distance = speed_m_s*2 # "two second between" rule of thumb
+        # self.braking_distance = self.speed_m_s*2 # "two second between" rule of thumb
         # braking_distance = speed_m_s*3 # "three second between" rule of thumb
 
+    def calculate_max_cars(self, time_period_h: float = 1.0) -> int:
+
         car_length = 4
-        total_gap = braking_distance + car_length  
-        cars_per_second_per_lane = speed_m_s / total_gap
+        total_gap = self.braking_distance + car_length  
+        cars_per_second_per_lane = self.speed_m_s / total_gap
 
         # Total cars over time period across all lanes
         total_seconds = time_period_h * 3600
@@ -37,20 +37,28 @@ class Highway:
         exponent = -1 * (0.0619719 * x - 5.21318)
         y: float = 100.8366/ (1 + math.exp(exponent))
         return y
+    
+    def get_braking_distance(self) -> float:
+        return self.braking_distance
+
 
     
 
  
 if __name__ == "__main__":
-    speeds = [v for v in range(10, 150, 10)]
+    speeds = [v for v in range(10, 151, 10)]
     cars: list[int] = []
     safety: list[float] = []
+    braking_distances: list[float] = []
 
     for v in speeds:
         hw = Highway(speed_limit=v)
         cars.append(hw.calculate_max_cars(1))
         safety.append(hw.get_safety_rating())
+        braking_distances.append(hw.get_braking_distance()/hw.speed_m_s)
     
+    print(braking_distances)
+
     # Plotting
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
