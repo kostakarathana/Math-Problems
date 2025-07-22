d28e3b94bdf847005bf87e39d6f1de2fadc21904
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-07-22 20:51:28 +1000

    fixed first ace

--- First Ace From Deck Problem/Deck.py
+++ First Ace From Deck Problem/Deck.py
@@ -3,30 +3,21 @@ import random as r
 class CardDeck:
     def __init__(self) -> None:
         self.total_cards: int = 52
-        self.card_types: list[str] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        self.cards: dict[str,int] = {
-            "A": 4,
-            "2": 4,
-            "3": 4,
-            "4": 4,
-            "5": 4,
-            "6": 4,
-            "7": 4,
-            "8": 4,
-            "9": 4,
-            "10": 4,
-            "K": 4,
-            "Q": 4,
-            "J": 4,
-        }
+        self.card_types: list[str] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] * 4
+
 
     def draw(self) -> str:
         card: str = r.choice(self.card_types)
 
-        self.cards[card] -= 1
-        if self.cards[card] == 0:
-            self.card_types.remove(card)
+        self.card_types.remove(card)
         return card
+    
+    # 52 cards
+    # 4 aces
+    # 48 non-aces
+    # possibilities = 1, 2, 3....
+    # 1 * 4/52          +            2 * 48/52 * 4/51           +               3 * 48/52 * 47/51 * 4/50                +               4 * 48/52 * 47/51 * 46/59 * 4/49
+
     
 
         
