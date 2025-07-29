7575727bae42eff367e8f0f7c2ef2e9b2b200863
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-07-29 20:07:01 +1000

    pirate

---
+++ Maps/README.md
@@ -0,0 +1,16 @@
+# Mapping for Henry
+
+## What u need
+
+run these in your terminal:
+- pip install geopandas
+- pip install folium
+- pip install mapclassify
+- pip install matplotlib
+
+if pip install doesn't work try pip3 install
+
+on the jupyter file, press run for the code I did. It should then display a map
+of provinces in China as an example. 
+
+
---
+++ Pirates Jumping Ship/main.py
@@ -0,0 +1,44 @@
+import random as r
+
+class Game:
+    def __init__(self, boats: int =7) -> None:
+        self.boats = boats
+        self.boat_choices = [i for i in range(1,self.boats+1)]
+        self.game_over = False
+        self.current_boat = r.choice(self.boat_choices)
+
+        self.record: list[str] = []
+        self.detailed_record: list[str] = []
+
+    def jump(self) -> None:
+        previous_boat: int = self.current_boat
+        self.boat_choices.remove(self.current_boat)
+        if self.boats == 1:
+            self.game_over = True
+            return
+        
+        self.current_boat = r.choice(self.boat_choices)
+        self.detailed_record.append(f"boat {previous_boat} -> boat {self.current_boat}")
+        if self.current_boat > previous_boat:
+            self.record.append("R")
+        else:
+            self.record.append("L")
+
+        self.boats -= 1
+
+if __name__ == "__main__":
+    simulations = 100000
+    number_of_single_lefts = 0
+
+
+    for i in range(simulations):
+        game = Game(10)
+        while not game.game_over:
+            game.jump()
+        if game.record.count("L") == 1:
+            number_of_single_lefts += 1
+    print(f"percentage where pirate jumped left exactly once: {number_of_single_lefts*100/simulations}")
+
+
+
+    
