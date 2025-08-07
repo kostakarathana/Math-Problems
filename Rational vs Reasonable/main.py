ab5144a178ee4fb757b57a9af7b464e60ffc35cb
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-08-07 13:16:48 +1000

    small changes

--- Rational vs Reasonable/main.py
+++ Rational vs Reasonable/main.py
@@ -1,30 +1,37 @@
 import random as r
+# import matplotlib.pyplot as plt
 
 class Bettor:
     def __init__(self) -> None:
-        self.balance: int = 0
+        self.balance: float = 0
 
-    def safe_bet(self) -> None:
+    def safe_bet(self) -> float:
         self.balance += 100
+        return self.balance
     
-    def risky_bet(self) -> None:
+    def risky_bet(self) -> float:
         if r.randint(1,1000) == 1:
-            return
+            pass
         else:
             self.balance += 101
+        return self.balance
     
-    def get_balance(self) -> int:
+    def get_balance(self) -> float:
         return self.balance
     
 if __name__ == "__main__":
-    safe_bettor = Bettor()
-    risky_bettor = Bettor()
+    safe_bettor: Bettor = Bettor()
+    risky_bettor: Bettor = Bettor()
+    SB_rec: list[float] = []
+    RB_rec: list[float] = []
+
 
     iterations = 1000
 
     for i in range(iterations):
-        safe_bettor.safe_bet()
-        risky_bettor.risky_bet()
+        SB_rec.append(safe_bettor.safe_bet())
+        RB_rec.append(risky_bettor.risky_bet())
+
     
-    print(safe_bettor.get_balance())
-    print(risky_bettor.get_balance())
+    
+
